5bd2e2cb35705082981a705dfce779e706a7464c
Murat Tokcanlı <96322566+mtkcnl@users.noreply.github.com>
2026-04-20 11:14:03 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,13 @@
+/**
+ * Fill buffer with rdrand instruction.
+ * @param buffer 
+ */
 export function fillBuffer(buffer:Buffer):void;
+/**
+ * rdrand instruction
+ */
 export function getRdrand():BigInt;
+/**
+ * rdseed instruction
+ */
 export function getRdseed():BigInt;
